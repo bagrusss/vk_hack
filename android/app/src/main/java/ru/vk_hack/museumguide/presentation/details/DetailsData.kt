c245bba8747a47b241963a4bfbee275a237eaf85
Vladislav Kozhushko <bagrusss@gmail.com>
2017-10-21 18:42:17 +0300

    прогресс

---
+++ android/app/src/main/java/ru/vk_hack/museumguide/presentation/details/DetailsData.kt
@@ -0,0 +1,11 @@
+package ru.vk_hack.museumguide.presentation.details
+
+import android.databinding.ObservableField
+
+/**
+ * Created by bagrusss on 21.10.17
+ */
+class DetailsData {
+    val author = ObservableField<String>()
+
+}
